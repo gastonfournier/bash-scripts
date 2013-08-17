f6fe399b39a94bd1d85d1cec758e58d71ada88d3
Gastón Fournier <gfournier@despegar.com>
2013-08-17 17:27:33 -0300

    Added the real file

--- mail/decode-utf-8-subject.py
+++ mail/decode-utf-8-subject.py
@@ -1,1 +1,9 @@
-/home/gaston/utils/mail/decode-utf-8-subject.py
+#!/usr/bin/python
+from email.header import decode_header
+import sys 
+
+if (len(sys.argv) > 1):
+    subject = sys.argv[1]
+    print decode_header(subject)[0][0]
+else:
+    print "USE: python "+sys.argv[0]+" \"=?UTF-8?Q?TEST:_T=C3=A9st_c=C3=B3n_=C3=A1c=C3=A9nt=C3=B3s?=\""
--- mail/dummy-mail-server.py
+++ mail/dummy-mail-server.py
@@ -1,1 +1,19 @@
-/home/gaston/utils/mail/dummy-mail-server.py
+#!/usr/bin/python
+#host = "ic.despegar.it.ar"
+import smtpd, asyncore
+port = 25
+
+# SMTP server class
+class CustomSMTPServer(smtpd.SMTPServer):
+    
+    def process_message(self, peer, mailfrom, rcpttos, data):
+        print 'Message from:', mailfrom
+        print 'Message to  :', rcpttos
+        print 'Message     :', data
+	print '-----------------------------------------------'
+        return
+
+#server = smtpd.DebuggingServer(('localhost', port), None)
+server = CustomSMTPServer(('127.0.0.1', port), None)
+print "Listening on port: "+str(port)
+asyncore.loop()
